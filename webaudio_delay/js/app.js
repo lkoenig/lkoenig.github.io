2e01fa2a9529a0330d6ba42082a2b68d256159e7
Lionel Koenig Gélas <lionelk@google.com>
2025-03-19 11:21:15 +0100

    Add support for getDisplayMedia

--- webaudio_delay/js/app.js
+++ webaudio_delay/js/app.js
@@ -7,6 +7,19 @@ import { getDataAsWav } from './wav_tools.js';
 
 const SAMPLE_RATE_HZ = 48000.0;
 
+const gumUseCheckbox = document.getElementById('gum-use');
+const gdmUseCheckbox = document.getElementById('gdm-use');
+
+const startElement = document.getElementById("measureButton");
+const plotCanvas = document.getElementById("plotCanvas");
+const downloadMeasurement = document.getElementById("downloadMeasurementLink");
+downloadMeasurement.hidden = true;
+const downloadLinearImpulseResponse = document.getElementById("downloadLinearImpulseResponseLink");
+downloadLinearImpulseResponse.hidden = true;
+const measuredResult = document.getElementById("measuredResult");
+const audioContextLatency = document.getElementById("audioContextLatency");
+
+
 let audioContext;
 
 const exponential_sine_sweep = new ExponentialSineSweep(20.0 / SAMPLE_RATE_HZ * 2.0 * Math.PI, 0.5 * 2.0 * Math.PI, 2 ** 18);
@@ -23,17 +36,6 @@ async function initializeAudio(onMeasurement) {
     }
     await audioContext.audioWorklet.addModule('js/measurement_audio_worklet.js');
 
-    // Get user's microphone and connect it to the AudioContext.
-    const inputStream = await navigator.mediaDevices.getUserMedia({
-        audio: {
-            echoCancellation: { exact: false },
-            autoGainControl: { exact: false },
-            noiseSuppression: { exact: false },
-            latency: 0
-        }
-    });
-
-    const inputNode = new MediaStreamAudioSourceNode(audioContext, { mediaStream: inputStream });
     const impulseResponseEstimatorNode = new AudioWorkletNode(
         audioContext,
         'measurement-processor',
@@ -43,25 +45,73 @@ async function initializeAudio(onMeasurement) {
             }
         }
     );
+    console.log('Connect stimulus to output of AudioContext');
+    impulseResponseEstimatorNode.connect(audioContext.destination);
+
+
+    let gdmInputStream;
+    if (gdmUseCheckbox.checked) {
+        console.log('Use getDisplayMedia');
+        const gdmOptions = {
+            video: true,
+            audio: {
+                echoCancellation: false,
+                autoGainControl: false,
+                noiseSuppression: false,
+                suppressLocalAudioPlayback: false,
+            },
+            systemAudio: 'include',
+            preferCurrentTab: false,
+            selfBrowserSurface: 'include',
+            surfaceSwitching: 'exclude',
+            monitorTypeSurfaces: 'include',
+
+        };
+        gdmInputStream = await navigator.mediaDevices.getDisplayMedia(gdmOptions);
+        console.log(gdmInputStream);
+        const gdmNode = new MediaStreamAudioSourceNode(audioContext, { mediaStream: gdmInputStream });
+        gdmNode.connect(impulseResponseEstimatorNode);
+    }
+
+    let gumInputStream;
+    if (gumUseCheckbox.checked) {
+        console.log('Use getUserMedia');
+        // Get user's microphone and connect it to the AudioContext.
+        gumInputStream = await navigator.mediaDevices.getUserMedia({
+            audio: {
+                echoCancellation: { exact: false },
+                autoGainControl: { exact: false },
+                noiseSuppression: { exact: false },
+                latency: 0
+            }
+        });
+
+        const gumNode = new MediaStreamAudioSourceNode(audioContext, { mediaStream: gumInputStream });
+        gumNode.connect(impulseResponseEstimatorNode);
+    }
 
     console.log("Register logger for the audio worklet");
-    console.log(`${MeasurementAudioMessageType.MEASUREMENT_DONE}`);
     impulseResponseEstimatorNode.port.onmessage = (event) => {
-        // console.log(event);
         if (event.data.type === MeasurementAudioMessageType.LOG) {
             console.log("[Worklet]: " + event.data.message);
         } else if (event.data.type === MeasurementAudioMessageType.MEASUREMENT_DONE) {
             console.log("Measurement is done");
-            inputStream.getTracks().forEach(function (track) {
-                track.stop();
-            });
+            if (gumInputStream !== undefined) {
+                gumInputStream.getTracks().forEach(function (track) {
+                    track.stop();
+                });
+            }
+            if (gdmInputStream !== undefined) {
+                gdmInputStream.getTracks().forEach(function (track) {
+                    track.stop();
+                });
+            }
             audioContext.suspend();
             onMeasurement(event.data.measurement);
         }
     };
 
-    inputNode.connect(impulseResponseEstimatorNode);
-    impulseResponseEstimatorNode.connect(audioContext.destination);
+    console.log('Start measurment');
     impulseResponseEstimatorNode.port.postMessage({
         type: MeasurementAudioMessageType.START_MEASUREMENT,
     });
@@ -72,16 +122,6 @@ function startMeasurement(onMeasurement) {
     console.log("Start measurement");
     initializeAudio(onMeasurement);
 }
-
-
-const startElement = document.getElementById("measureButton");
-const plotCanvas = document.getElementById("plotCanvas");
-const downloadMeasurement = document.getElementById("downloadMeasurementLink");
-downloadMeasurement.hidden = true;
-const downloadLinearImpulseResponse = document.getElementById("downloadLinearImpulseResponseLink");
-downloadLinearImpulseResponse.hidden = true;
-const measuredResult = document.getElementById("measuredResult");
-const audioContextLatency = document.getElementById("audioContextLatency");
 
 const p = new Plot(plotCanvas);
 
